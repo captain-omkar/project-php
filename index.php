179f98b7107b424beb59a2058acc07dbe870d31d
captain-omkar <112746387+captain-omkar@users.noreply.github.com>
2022-10-13 12:09:27 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <body>
 
-<h1>My first PhhHP pages in thcnnnnnndsdvcevvv freestyle-2</h1>
+<h1>My first PhhHP pages in thcnnnnnndnnnsdvcevvv freestyle-2</h1>
 
 <?php
 echo "Hello deeeWorld!";
